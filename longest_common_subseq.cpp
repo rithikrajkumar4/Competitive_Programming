0a70c9b3d75738e37346a8d4155fe933fae8c21c
Rithik Rajkumar <rithikrajkumar@gmail.com>
2022-01-07 02:57:33 +0530

    noyce

--- longest_common_subseq.cpp
+++ longest_common_subseq.cpp
@@ -38,7 +38,26 @@ using namespace std;
 int static dp[1001][1001];
 
 void solve(){
-	 
+	string x,y;cin>>x>>y;
+	int n = x.length(),m = y.length();
+	for(int i=0;i<=n;i++)dp[i][0] = 0;
+	for(int j=0;j<=m;j++)dp[0][j] = 0;
+
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			if(x[i] == y[j])dp[i][j] = dp[i-1][j-1]+1;
+			else{
+				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+			}
+		}
+	}
+	for(int i=0;i<=n;i++){
+		for(int j=0;j<=m;j++){
+			cout<<dp[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	cout<<dp[n][m];
 }
 
 int main(){
